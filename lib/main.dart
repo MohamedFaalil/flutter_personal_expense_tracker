92fe84fe8ef9680cca52ed16238bbd73012b2c4b
Faalil <mohamedfaalil05@gmail.com>
2022-08-24 23:52:51 +0530

    basic transaction card is shown

--- lib/main.dart
+++ lib/main.dart
@@ -60,8 +60,22 @@ class MyHomePage extends StatelessWidget {
           Column(
             children: transactions.map((Transaction tx) {
               return Card(
-                child: Text(
-                  tx.title,
+                child: Row(
+                  children: [
+                    Text(
+                      tx.amount.toString(),
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          tx.title,
+                        ),
+                        Text(
+                          tx.date.toString(),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               );
             }).toList(),
